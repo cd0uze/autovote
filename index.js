5433e2a84ba5131459c8481f7d09cba86373ae41
cdouze <fofbob265@gmail.com>
2024-08-19 18:02:59 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ puppeteer.use(StealthPlugin());
 const Ext = path.join(process.cwd(), "./0.4.12_0");
 
 async function sleep(ms){
-    return new Promise(resolve => setTimeout(resolve, ms))
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 const browser = await puppeteer.launch({
