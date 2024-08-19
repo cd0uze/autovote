b28237d23474cd16218f77caf71d60774752eb7b
cdouze <fofbob265@gmail.com>
2024-08-19 15:09:44 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const browser = await puppeteer.launch({
   targetFilter: null
 }).catch(err => console.log(err)),
           browser2 = await puppeteer.launch({
-  headless: false,
+  headless: true,
   args: [
     `--disable-extensions-except=${Ext}`, 
     `--load-extension=${Ext}`,
