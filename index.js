ccb004df4646789ed1d2a978d0505be50e808391
cdouze <fofbob265@gmail.com>
2024-08-21 00:05:21 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const response = await connect({
     turnstile: true,
 });
 
-const {page0, browser} = await response,
+const {page, browser} = await response,
 browser2 = await puppeteer.launch({
   headless: false,
   args: [
