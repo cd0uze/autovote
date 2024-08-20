a5c5acc4552d063959ec7caefb203b8727675c21
cdouze <fofbob265@gmail.com>
2024-08-20 20:28:08 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -142,7 +142,6 @@ if(Config.sites[i].cloudflare){
 
                 if(Url === Config.sites[i].voteUrl) {
                     console.log("Website " + Config.sites[i].index + " | Vote added !");
-                    resolve()
                 } else {
                     console.log("Website " + Config.sites[i].index + " | Unknown error !");
                     resolve();
