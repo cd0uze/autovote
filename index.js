832f38c2451e6da97fd9d6249ccda74b20aba0fb
cdouze <fofbob265@gmail.com>
2024-08-21 00:01:33 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ browser2 = await puppeteer.launch({
   targetFilter: null
 }).catch(err => console.log(err));
 
-page.goto("https://google.com")
 
 async function autovote(i) {
     await (Config.sites[i].turnstile ? browser : browser2).newPage().then(async page0 => {
