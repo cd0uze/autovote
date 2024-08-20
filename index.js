9a2e62a85cd463e0b7e254a6b3217bd0dd681769
cdouze <fofbob265@gmail.com>
2024-08-20 20:28:41 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ if(Config.sites[i].cloudflare){
                 console.log("Website " + Config.sites[i].index + " | Vote added !");
             } else if(Result === "We cannot verify your vote due to a low browser score. Try another browser or try login to Google to raise your score." || Result === "The verification expired due to timeout.Simply click the Vote button again and it should work."){
                     console.log("Website " + Config.sites[i].index + " | Error occured.Voting again...");
-                    check()
+                    await check()
                     } else {
                         console.log("Website " + Config.sites[i].index + " | Unknown error !");
                     }
