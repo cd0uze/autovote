c59468608101832a719a9a44bcd05a4cbb3a2d71
cdouze <fofbob265@gmail.com>
2024-08-20 22:04:10 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -136,6 +136,7 @@ if(Config.sites[i].cloudflare){
                     resolve()
                 } else {
                     console.log("Website " + Config.sites[i].index + " | Unknown error !");
+                    resolve()
                 }
             }
 
