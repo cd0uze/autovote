4b72ad161c6574fa572548ddbe703be44a31caef
cdouze <fofbob265@gmail.com>
2024-08-20 23:58:01 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function autovote(i) {
     await (Config.sites[i].turnstile ? browser : browser2).newPage().then(async page0 => {
         page0 = Config.sites[i].turnstile ? page : page0
 
-    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36');
+await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36');
 
 console.log("Website " + Config.sites[i].index + " | Opening " + Config.sites[i].url+ "...");
 
