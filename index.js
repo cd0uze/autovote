4db06139b9462eeea08c6e128f0784491adafeea
cdouze <fofbob265@gmail.com>
2024-08-20 19:13:46 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function sleep(ms){
 };
 
 const browser = await puppeteer.launch({
-  headless: true,
+  headless: false,
   args: [
     `--disable-extensions-except=${Ext}`, 
     `--load-extension=${Ext}`,
