b0763407eb64fa798d42d88aee376484463aa6d5
cdouze <fofbob265@gmail.com>
2024-08-20 23:57:20 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -31,8 +31,8 @@ browser2 = await puppeteer.launch({
 
 
 async function autovote(i) {
-    await (Config.sites[i].turnstile ? browser : browser2).newPage().then(async page => {
-        
+    await (Config.sites[i].turnstile ? browser : browser2).newPage().then(async page0 => {
+
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36');
 
 console.log("Website " + Config.sites[i].index + " | Opening " + Config.sites[i].url+ "...");
