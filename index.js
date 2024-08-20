666979259dd0bc272e0eb184f8152df304c91c38
cdouze <fofbob265@gmail.com>
2024-08-20 20:30:52 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -149,6 +149,7 @@ if(Config.sites[i].cloudflare){
 
                 if(Url === Config.sites[i].voteUrl) {
                     console.log("Website " + Config.sites[i].index + " | Vote added !");
+                    Voted = true;
                 } else {
                     console.log("Website " + Config.sites[i].index + " | Unknown error !");
                 }
