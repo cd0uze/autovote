61647b5395d393b1e169b263e94ed303fc341000
cdouze <fofbob265@gmail.com>
2024-08-20 22:59:59 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -127,6 +127,7 @@ if(Config.sites[i].cloudflare){
 
                 if(Config.sites[i].index == 4) {
                 await page.waitForNavigation();
+                
                 await page.waitForSelector("div[class='modal-body text-center']", {timeout: 0});
 
                 const Result = await page.evaluate(() => document.getElementsByClassName("modal-body text-center")[0]?.textContent);
