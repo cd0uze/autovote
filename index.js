37e2157d8cd97a1451b28985524d15b7f872be8f
cdouze <fofbob265@gmail.com>
2024-08-20 19:13:00 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ if(Config.sites[i].cloudflare){
                 await page.waitForNavigation({timeout: 0});
 
                 const Url = await page.url();
-                console.log()
+                console.log("Website " + Config.sites[i].index + " | Vote added !")
 
                 if(Url === Config.sites[i].voteUrl) {
                     console.log("Website " + Config.sites[i].index + " | Vote added !");
