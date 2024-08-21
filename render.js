cee97c511176343b10e0f0b97446677396ac73e9
cdouze <fofbob265@gmail.com>
2024-08-21 20:37:42 +0100

    Auto-committing 'render.js'

--- render.js
+++ render.js
@@ -1,7 +1,7 @@
 if (process.platform !== "win32") {
     import express from "express"
 
-    const app = express()
+    const app = express();
     const port = 3000
     
     app.use(express.static("screenshots"));
