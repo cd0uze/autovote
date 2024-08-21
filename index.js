c6504760eb4da5237d80fb6e4414399c73361f8d
cdouze <fofbob265@gmail.com>
2024-08-21 21:05:49 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (process.platform !== "win32") {
     const port = 3000;
     
     app.get('/', (req, res) => {
-      res.send("Testing")
+      res.send("Testing...")
     })
     
     app.listen(port, () => {
