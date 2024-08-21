ba36c7d57e21e48bd382722e5abb4521df1bcf99
cdouze <fofbob265@gmail.com>
2024-08-21 21:05:20 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ import { connect } from 'puppeteer-real-browser';
 import path from 'path';
 import Config from './config.json' with { type: "json" };
 
+if (process.platform !== "win32") {
+    import express from "express";
+
+    const app = express();
+    const port = 3000;
+    
+    
+    app.get('/', (req, res) => {
+      res.send('sltcv')
+    })
+    
+    app.listen(port, () => {
+      console.log(port)
+    })
+    
+    function get() {
+      https.get("https://autovote.onrender.com");
+    }
+    
+    setInterval(get, 300000);
+}
+
 puppeteer.use(StealthPlugin());
 
 const Ext = path.join(process.cwd(), "./0.4.12_0");
