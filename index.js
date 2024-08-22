25a56b26639a96e9a735159781eb9c5580b46dd6
cdouze <fofbob265@gmail.com>
2024-08-22 16:16:04 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ if(Config.sites[i].cloudflare){
         if(Config.sites[i].index == 1){
             await page.waitForSelector("button[class='btn btn-primary btn-lg btn-block']", {timeout: 0});
 
-            const Result = await page.evaluate(() => document.getElementsByClassName("btn btn-primary btn-lg btn-block"))
+            const Result = await page.evaluate(() => document.getElementsByClassName("btn btn-primary btn-lg btn-block")[0].textContent);
         }
                 if(Config.sites[i].index == 4) {
                 await page.waitForSelector("div[class='modal-body text-center']", {timeout: 0});
