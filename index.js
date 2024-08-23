61f581aad58ce5455f29635f3b40626299ba5587
cdouze <fofbob265@gmail.com>
2024-08-23 15:03:25 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -127,8 +127,8 @@ if(Config.sites[i].cloudflare){
 
         console.log("Website " + Config.sites[i].index + " | Waiting for vote...");
 
-        if(Config.sites[i].index !== 1 && Config.sites[i].index !== 2 && Config.sites[i].index !== 7) await page.waitForNavigation({timeout: 0});
-        
+        if(Config.sites[i].index !== 2 && Config.sites[i].index !== 7) await page.waitForNavigation({timeout: 0});
+
                 if(Config.sites[i].index == 4) {
                 await page.waitForSelector("div[class='modal-body text-center']", {timeout: 0});
 
