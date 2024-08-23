45fc84619b3d215e016c615d16d9f0e9d5f9e30b
cdouze <fofbob265@gmail.com>
2024-08-23 15:28:15 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -132,8 +132,8 @@ if(Config.sites[i].cloudflare){
         if(Config.sites[i].index == 1) {
             console.log((await page.url()));
             await page.waitForSelector("button[class='btn btn-primary btn-lg btn-block']", {timeout: 0}).catch(async err => {
-                
-
+                console.log("Website " + Config.sites[i].index + " | Unknown error !");
+                resolve();
             })
 
             const Result = await page.evaluate(() => document.getElementsByClassName("btn btn-primary btn-lg btn-block")[0]?.textContent);
