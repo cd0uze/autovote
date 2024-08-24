2d26f4035a683703b5a6a6f8f0eeeb0bd5b48848
cdouze <fofbob265@gmail.com>
2024-08-24 21:04:59 +0100

    Auto-committing 'render.js'

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { loop } from './index.js' with { type: "js" };
+import { loop } from './index.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
