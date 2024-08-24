5266d755d2bec241954e3b8a9c33b14804f2f357
cdouze <fofbob265@gmail.com>
2024-08-24 21:02:18 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ await check();
 }).catch(err => console.log("Website " + Config.sites[i].index + " | " + err.message));
 };
 
-async function loop() {
+const loop = async function {
 for (const i in Config.sites) {
     await autovote(i);
 };
