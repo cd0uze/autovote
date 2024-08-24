cdda544173371cfca11dc4784ef55a16df826a91
cdouze <fofbob265@gmail.com>
2024-08-24 20:36:43 +0100

    Auto-committing 'render.js'

--- render.js
+++ render.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { scrapeLog } = require("./scrapeLogic");
+const {  } = require("./scrapeLogic");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
