0aac3b9512a758478cca1ab68139c0e675dacb6a
cdouze <fofbob265@gmail.com>
2024-08-25 23:09:51 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -36,7 +36,8 @@ browser2 = await puppeteer.launch({
   targetFilter: null
 }).catch(err => console.log(err));
 
-page.close();
+await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36');
+        await page.setDefaultNavigationTimeout(0);
 setTarget({status: false});
 
 async function autovote(i) {
