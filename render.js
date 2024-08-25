84d22c90cdb433e91eb30899ac3b18f37a0c4883
cdouze <fofbob265@gmail.com>
2024-08-25 23:09:27 +0100

    Auto-committing 'render.js'

--- render.js
+++ render.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {jsp} from './index.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Autovote is running now !")
