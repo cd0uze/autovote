aa2f224808ea6c699918311405666fd8bd25dd23
cdouze <fofbob265@gmail.com>
2024-08-28 00:00:10 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const response = await connect({
 
 const {page, browser, setTarget} = response,
 browser2 = await puppeteer.launch({
+  timeout: 0,
   headless: true,
   executablePath: '/usr/bin/chromium-browser',
   args: [
