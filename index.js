c764d34d62f4a57502780feba97ba637b7d819ff
cdouze <fofbob265@gmail.com>
2024-08-29 00:57:29 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ if(Config.sites[i].cloudflare){
 await check();
 }, 5000);
 }).then(async () => {
-    await page.waitForNetworkIdle({timeout: 0})
+    await page.waitForNetworkIdle({timeout: 0});
     await page.close().catch();
 }).catch(err => console.log("Website " + Config.sites[i].index + " | " + err.message));
 }).catch(err => console.log("Website " + Config.sites[i].index + " | " + err.message));
