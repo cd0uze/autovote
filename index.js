5adcc03bd9e2c9f64f12aa965ab6acc31abcbca1
cdouze <fofbob265@gmail.com>
2024-08-29 00:53:16 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ if(Config.sites[i].cloudflare){
 
         console.log("Website " + Config.sites[i].index + " | Waiting for vote...");
 
-        if(![1, 2, 7].includes(Config.sites[i].index)) await page.waitForNavigation({timeout: 0});
+        await page.waitForNetworkIdle()({timeout: 0});
 
         if(Config.sites[i].index == 1) {
             const Interval4 = setInterval(async function() {
