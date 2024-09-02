37da2a7edb716b46169648a4f25fa67af94196a1
cdouze <fofbob265@gmail.com>
2024-09-03 00:31:38 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function autovote(i) {
 
         console.log([1, 6].includes(Config.sites[i].index))
 
-        if([1, 6].includes(Config.sites[i].index)) {
+        if([1, 6].includes(Config.sites[i].index)){
             await page.waitForFunction('document.title.includes("Vote")', {timeout: 0});
         }
 
