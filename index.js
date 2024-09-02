9958ebeb458da7e933ece329619af7ca7d487c9b
cdouze <fofbob265@gmail.com>
2024-09-02 23:50:34 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ async function autovote(i) {
         await page.goto(Config.sites[i].url, {waitUntil: "networkidle0", timeout: 0});
 
         if([1, 6].includes(Config.sites[i].index)) {
-            page.waitForFunction('page.evaluate(() => !document.title.includes("Un instant") || !document.title.includes("Wait")')
+            page.waitForFunction('page.evaluate(() => !document.title.includes("Un instant") || !document.title.includes("Wait")', timeout{})
         }
 
         console.log(clc.green("Website " + Config.sites[i].index + " | Website opened !"));
