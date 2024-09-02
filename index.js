a9298757b3ed9ecb5823f9bd73457e94b4c3d17c
cdouze <fofbob265@gmail.com>
2024-09-03 00:28:10 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ async function autovote(i) {
                             const Result = await page.evaluate(() => document.getElementsByClassName("modal-body text-center")[0]?.textContent);
 
                             if(Result.includes("Thank you for voting!")) {
-                                resolve(clc.green("Website " + Config.sites[i].index + " | Vote added !"));                           
+                                resolve();                           
                             } else {
                                 reject();
                             }
