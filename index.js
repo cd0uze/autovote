b3220450163dba349e360ac9a3f6142500b7e91b
cdouze <fofbob265@gmail.com>
2024-09-08 21:44:56 +0100

    Auto-committing 'index.js'

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer-extra';
+import puppeteer from 'puppeteer-core';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import { connect } from 'puppeteer-real-browser-nopecha';
 import path from 'path';
